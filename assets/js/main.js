66bac6e16744a026819a678b27fd11d96341f097
Anabel Sposato <sposatoanabel@gmail.com>
2017-07-26 11:55:22 +0100

    dropdown menus fixed

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by asposato on 26/07/2017.
+ */
+$(window).resize(function () {
+    var viewportWidth = $(window).width();
+    if (viewportWidth < 767) {
+        $("#topNav").hide();
+        $("#btnSubscribe").removeClass("pull-right").addClass("pull-left");
+
+    } else if(viewportWidth > 767) {
+        $("#topNav").show();
+    }
+});
+
+$(document).ready(function(){
+    $('#bottomNav').affix({offset: {top: 64} });
+});
--- vendors/js/offcanvas/offcanvas.js
+++ vendors/js/offcanvas/offcanvas.js
@@ -5,9 +5,9 @@ $(document).ready(function () {
 
 
     //stick in the fixed 100% height behind the navbar but don't wrap it
-    $('#slide-nav.navbar-inverse').after($('<div class="inverse" id="navbar-height-col"></div>'));
+    $('#bottomNav.navbar-inverse').after($('<div class="inverse" id="navbar-height-col"></div>'));
 
-    $('#slide-nav.navbar-default').after($('<div id="navbar-height-col"></div>'));
+    $('#bottomNav.navbar-default').after($('<div id="navbar-height-col"></div>'));
 
     // Enter your ids or classes
     var toggler = '.navbar-toggle';
@@ -19,7 +19,7 @@ $(document).ready(function () {
     var slideneg = '-80%';
 
 
-    $("#slide-nav").on("click", toggler, function (e) {
+    $("#bottomNav").on("click", toggler, function (e) {
 
         var selected = $(this).hasClass('slide-active');
 
