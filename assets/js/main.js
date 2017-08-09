6a93d9a6cb9383569c2f5c90465e98de4578bb3a
Anabel Sposato <sposatoanabel@gmail.com>
2017-08-09 15:49:48 +0100

    media queries/ css amendments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,62 @@
 /**
  * Created by asposato on 26/07/2017.
  */
-$(window).resize(function () {
-    var viewportWidth = $(window).width();
-    if (viewportWidth < 767) {
-        $("#topNav").hide();
-        $("#btnSubscribe").removeClass("pull-right").addClass("pull-left");
+(function ($) {
 
-    } else if(viewportWidth > 767) {
-        $("#topNav").show();
-    }
-});
+    var resizeNavBar = function () {
 
-$(document).ready(function(){
-    $('#bottomNav').affix({offset: {top: 64} });
-    $('#nav-icon1').click(function () {
-        $(this).toggleClass('open');
-    });
+            $(window).resize(function () {
+                var viewportWidth = $(window).width();
+                if (viewportWidth < 767) {
+                    $("#topNav").hide();
+                    $("#btnSubscribe").removeClass("pull-right").addClass("pull-left");
 
+                } else if (viewportWidth > 767) {
+                    $("#topNav").show();
+                }
+            });
+        }, //resizeNavBar()
 
-    $("#readMoreBtn").click(function () {
+        affixBottomNav = function () {
+            $(document).ready(function () {
+                $('#bottomNav').affix({offset: {top: 64}});
+            });
 
-        var readMoreTextElement = $(this).text();
-        if (readMoreTextElement == "Read More") {
-            $(this).text("Read Less");
-            $("#infoMore").slideDown();
-        } else {
-            $(this).text("Read More");
-            $("#infoMore").slideUp();
+        }, // affixBottomNav()
+
+        switchClassHamIcon =
+            function () {
+                $(document).ready(function () {
+                    $('#nav-icon1').click(function () {
+                        $(this).toggleClass('open');
+                    });
+
+                });
+            }, //switchClassHamIcon()
+
+        bindReadMoreButton = function () {
+            $("#readMoreBtn").click(function () {
+
+                var readMoreTextElement = $(this).text();
+                if (readMoreTextElement === "Read More") {
+                    $(this).text("Read Less");
+                    $("#infoMore").slideDown();
+                } else {
+                    $(this).text("Read More");
+                    $("#infoMore").slideUp();
+                }
+            });
+
+        }, // bindReadMoreButton()
+
+        init = function () {
+            resizeNavBar();
+            switchClassHamIcon();
+            affixBottomNav();
+            bindReadMoreButton();
         }
-    });
+    ; // init()
 
-});
+    window.addEventListener("load", init);
 
+})(jQuery); // IIFE function
